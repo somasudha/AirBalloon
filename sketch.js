cb8f51139e5e8d1c9e8e72c2ad64656c7c33df33
somasudha <somasudha@gmail.com>
2021-07-01 16:23:12 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,92 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database,position;
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+
+  var balloonref = database.ref('Balloon/Position');
+  balloonref.on("value", readHeight,showError);
 }
 
+// function to display UI
+
+
+
 function draw() {
-  background(255,255,255);  
+  background(bg);
+
+  
+
+
+  if(keyDown(LEFT_ARROW)){
+   
+    //write code to move air balloon in left direction
+    updateHeight(-10,0)
+    
+    
+    
+  }
+  else if(keyDown(RIGHT_ARROW)){
+   
+    //write code to move air balloon in right direction
+    updateHeight(10,0)
+    
+  }
+  else if(keyDown(UP_ARROW)){
+    
+    //write code to move air balloon in up direction
+    updateHeight(0,-10)
+   
+    balloon.scale = balloon.scale -0.01
+  }
+  else if(keyDown(DOWN_ARROW)){
+   
+    //write code to move air balloon in down direction
+    updateHeight(0,10)
+   
+    balloon.scale = balloon.scale + 0.01
+  }
+
   drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("Instructions: Use the arrow keys to move!",40,40);
 }
+
+function updateHeight(x,y)
+{
+  database.ref('Balloon/Position').update({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readHeight(data)
+{
+  position = data.val();
+  console.log(position.x);
+  balloon.x = position.x;
+  balloon.y = position.y;
+}
+
+function showError()
+{
+    console.log("Error in writing the database");
